cbfd9a3c7f750b47366bd311ae0454495fcec8bd
이기현 <gihyeon.lee@goorm.io>
2020-02-09 14:11:51 +0000

    build: add initApp.sh

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,18 +3,9 @@ NODE_VER = "node10.16.3"
 node {
 	stage('Init') {
 		checkout scm
-		nodejs(NODE_VER) {
-			sh "npm install"
-		}
 	}
 
-	stage('Build') {
-		nodejs(NODE_VER) {
-			sh "npm run build"
-		}
-	}
-	
-	stage('Build') {
+	stage('Deploy') {
 		withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
 			step([$class: 'AWSCodeDeployPublisher', applicationName: 'basic-web-deploy', awsAccessKey: AWS_ACCESS_KEY_ID, awsSecretKey: AWS_SECRET_ACCESS_KEY, credentials: 'awsAccessKey', deploymentGroupAppspec: false, deploymentGroupName: 'basic-web-dg', deploymentMethod: 'deploy', excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'ap-northeast-2', s3bucket: 'basic-web-bin', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
 		}
