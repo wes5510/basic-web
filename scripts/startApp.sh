73d488e942f5c0cb0e29a976aee820c1c8fcac96
이기현 <gihyeon.lee@goorm.io>
2020-02-09 14:06:59 +0000

    fix scripts

--- scripts/startApp.sh
+++ scripts/startApp.sh
@@ -1,1 +1,4 @@
-cross-env NODE_ENV=production forever ../app.js
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cross-env NODE_ENV=production forever $DIR/../app.js
