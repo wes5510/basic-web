b94c6cf90c7736a07119156e59e9bf2e4b063ffa
이기현 <gihyeon.lee@goorm.io>
2020-02-09 12:04:02 +0000

    fix(jenkins): update aws info

--- jenkins.groovy
+++ jenkins.groovy
@@ -16,7 +16,7 @@ node {
 	
 	stage('Build') {
 		withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'jenkins', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-			step([$class: 'AWSCodeDeployPublisher', applicationName: 'basic-web', awsAccessKey: AWS_ACCESS_KEY_ID, awsSecretKey: AWS_SECRET_ACCESS_KEY, credentials: 'awsAccessKey', deploymentGroupAppspec: false, deploymentGroupName: 'test', deploymentMethod: 'deploy', excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'ap-northeast-1', s3bucket: '', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
+			step([$class: 'AWSCodeDeployPublisher', applicationName: 'basic-web-deploy', awsAccessKey: AWS_ACCESS_KEY_ID, awsSecretKey: AWS_SECRET_ACCESS_KEY, credentials: 'awsAccessKey', deploymentGroupAppspec: false, deploymentGroupName: 'basic-web-dg', deploymentMethod: 'deploy', excludes: '', iamRoleArn: '', includes: '**', proxyHost: '', proxyPort: 0, region: 'ap-northeast-2', s3bucket: 'basic-web-bin', s3prefix: '', subdirectory: '', versionFileName: '', waitForCompletion: false])
 		}
 		
 	}
